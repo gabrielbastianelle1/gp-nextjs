b93f24bf6fd9f477b977384e2ed3f034e5a427fd
Gabriel Bastianelle <gsbastianelle@gmail.com>
2022-12-27 17:49:15 +0000

    tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                colorButton: 'var(--color-button)',
+                userBackground: 'var(--color-background)',
+                headerList: 'var(--color-headerlist)'
+            }
+        }
+    },
+    plugins: []
+}
