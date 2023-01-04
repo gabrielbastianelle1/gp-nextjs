9d07931a7a84c161518b352737d089f1bae70f94
Gabriel Bastianelle <gsbastianelle@gmail.com>
2023-01-04 10:57:28 +0000

    fix total registers

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -4,11 +4,15 @@ import { getCurrentUser } from '../../services/service.auth'
 import Button from '../Button'
 
 export default function Profile() {
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState(undefined)
 
     useEffect(() => {
         getCurrentUser().then((response) => setUser(response.data.user))
     }, [])
+
+    if (user == undefined) {
+        return <div>loading</div>
+    }
 
     return (
         <main className="bg-userBackground flex-grow">
@@ -18,7 +22,7 @@ export default function Profile() {
                 <p className="item-list">Nome: {user.nome}</p>
                 <p className="item-list">Idade: {user.idade}</p>
                 <p className="item-list">Peso: {user.peso}</p>
-                <p className="item-list">Total registos: 10</p>
+                <p className="item-list">Total registos: {user.dados.length}</p>
                 <Button className="w-2/3 self-center bg-sincronizar text-white p-1">
                     Salvar
                 </Button>
